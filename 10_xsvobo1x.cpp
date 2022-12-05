e010c080383aa0402725d2ce60b22d34eeb0303c
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2022-12-05 11:31:05 +0100

    update

--- 10_xsvobo1x.cpp
+++ 10_xsvobo1x.cpp
@@ -31,14 +31,6 @@ https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3592192/pdf/586_2004_Article_755.pd
 In contrast, if no hemodilution were
 used during the procedure, then the hematocrit following operative blood loss would be approximately 28%
 */
-struct OperationWithoutANH {
-  Integrator vl;
-  OperationWithoutANH(Input hematocrit, double bloodVolume, double initialHematocrit) :
-    vl( - (hematocrit / bloodVolume), initialHematocrit) {}
-    double Value() {
-      return vl.Value();
-    }
-};
 
 struct ANH {
   Integrator vl;
@@ -48,49 +40,6 @@ struct ANH {
       return vl.Value();
     }
 };
-
-/*
-struct ANH : public aContiBlock {                  // wheel model
-  Integrator y;
-  ANH(Input Hct, double EBV, int Initial):
-	y( - ( Hct / EBV), Initial ) {
-    printf("Init: %d\n", Initial);
-  }    // height
-  double Value () {
-    return y.Value();
-    printf("ANH y: %f\n", y.Value());
-  }
-};
-*/
-
-struct ANH1 : public aContiBlock {                  // wheel model
-  Integrator y;
-  ANH1(Input Hct, double EBV, int Initial):
-	y( - ( Hct / EBV) - 5, Initial ) {
-    printf("Init: %d\n", Initial);
-    printf("ANH1 y: %f\n", y.Value());
-  }    // height
-  double Value () {
-    return y.Value();
-  }
-};
-
-
-// zredeni krve roztokem
-struct Hemodilution : public aContiBlock {
-  Hemodilution() {
-    printf("HEYO\n");
-  }
-  int computeValue (double hematocritBefore, double bloodVolume, double bloodToTake) {
-    double hematocritAfter = ((hematocritBefore * (bloodVolume - bloodToTake)) / bloodVolume);
-    printf("hematocritAfter: %f\n", hematocritAfter);
-    return hematocritAfter;
-  }
-  double Value () {
-    return 42;    // FIXME
-  }
-};
-
 
 // doplneni krve
 struct HemodilutionWithdrawn : public aContiBlock {
@@ -119,41 +68,6 @@ struct HemodilutionReplace : public aContiBlock {
   }
 };
 
-
-struct ANHadd : public aContiBlock {                  // wheel model
-  Integrator y;
-  //Hemodilution hd;
-  ANHadd(Input Hct, double EBV, int Initial):
-	//y( - ( Hct / EBV) + 20, Initial ) {
-  y( - ( Hct / EBV) + 20, Initial ) {
-    printf("Init: %d\n", Initial);
-    printf("y: %f\n", y.Value());
-  }    // height
-  double Value () {
-    return y.Value();
-  }
-};
-
-
-// objects
-//Constant Hct(40);             // constant input force
-//ANH anh (HtcI, BV, HtcI);           // model
-//ANH1 anh2(60, 10.0, 40);
-
-//Hemodilution hd;
-//ANHadd anhadd(hd.computeValue(28.0, BV, BT), 5.0, 28);
-//ANH anh3(hd.computeValue(28.0, BV, BT), 5.0, 28);
-
-// run operation without normovolemic hemodilution
-//OperationWithoutANH operationWithoutANH(HtcI, BV, HtcI);
-
-// run operation without normovolemic hemodilution
-//OperationWithoutANH operationWithoutANH2(25, 5.0, 25);
-
-//ANHadd anhadd(18, 3.5, 18);
-
-//ANHdoplneni anh4()
-
 // operation without anh - no hemodilution, experiment 0 and 1
 ANH anh01(40, 5.0, 40.0);
 // experiment 1, part 2
@@ -161,6 +75,10 @@ ANH anh2(32, 5.0, 32.0);
 // experiment 1, part 3
 ANH anh3(32.65, 4.5, 32.65);
 
+// experiment 2
+ANH anh4(32, 5.0, 32.0);
+
+ANH anh6(28, 5.0, 28.0);
 // neco dalsiho - jineho
 //ANH anh1(40, 5.0, 40.0);
 
@@ -169,30 +87,28 @@ HemodilutionWithdrawn hemodilutionWithdrawn;
 
 // print model state
 void Sample() {
-  //if (T.Value() < BL) {
-	//	Print("%6.3f %.4g\n", T.Value(),  anhadd.y.Value()); 
-  //} else {
-  //  Print("%6.3f %.4g\n", T.Value(),  anh2.y.Value()); 
-  //}
-  if (outputSelect == 0) {    // operation without hemodilution
-    //Print("%6.3f %.4g\n", T.Value(),  operationWithoutANH.Value());
+  if (outputSelect < 2) {    // operation without hemodilution
     Print("%6.3f %.4g\n", T.Value(), anh01.Value());
-  } else if (outputSelect == 1) {
-    Print("%6.3f %.4g\n", T.Value(), anh01.Value());
-    //Print("%6.3f %.4g\n", T.Value(),  operationWithoutANH2.Value()); 
   } else if (outputSelect == 2) {
     Print("%6.3f %.4g\n", T.Value(), anh2.Value());
-    //Print("%6.3f %.4g\n", T.Value(),  anhadd.y.Value()); 
-  } else if (outputSelect == 4) {
+  } else if (outputSelect == 3) {
     Print("%6.3f %.4g\n", T.Value(), anh3.Value());
+  } else if (outputSelect == 4) {
+    Print("%6.3f %.4g\n", T.Value(), anh4.Value());
+  } else if (outputSelect == 5) {
+    Print("%6.3f %.4g\n", T.Value(), anh01.Value());
+  } else if (outputSelect == 6) {
+    Print("%6.3f %.4g\n", T.Value(), anh6.Value());
+  } else if (outputSelect == 7) {
+    Print("%6.3f %.4g\n", T.Value(), anh01.Value());
   }
 }
 Sampler S(Sample, 0.001);       // periodic sampling
 
 int main() {                    // exeriment description
   // experiment 0: operation without hemodilution
-  /*
   outputSelect = 0;
+  printf("experiment 0: operation without hemodilution, 1.5l blood loss, 40%% initial hematocrit...\n");
   SetOutput("10_xsvobo1x_0.dat");
   Print("# Acute Normovolemic Hemodilution - experiment 0\n");
   Print("# BloodVolumeLoss   Hematocrit \n");
@@ -200,47 +116,91 @@ int main() {                    // exeriment description
   SetStep(1e-3, 0.1);           // numerical method step size range
   SetAccuracy(1e-5, 0.001);     // allowed (abs.+rel.) error tolerance
   Run();                        // simulation run
-  
+
   // experiment 1: 
   outputSelect = 1;
+  printf("experiment 1: operation with ANH, 1.5l blood loss, 40%% initial hematocrit:\n");
+  printf("part 1: ANH blood withdrawal, 1 litre...\n");
   SetOutput("10_xsvobo1x_1.dat");
   Print("# Acute Normovolemic Hemodilution\n");
   Print("# BloodVolumeLoss   Hematocrit \n");
   Init(0, 1.0);                 // set expected blood volume loss range [l]
   Run();                        // simulation run
-  */
 
   // experiment 1_1: 
   outputSelect = 2;
+  printf("part 2: operation after ANH, 1.5 litres blood loss...\n");
   SetOutput("10_xsvobo1x_2.dat");
   Print("# Acute Normovolemic Hemodilution\n");
   Print("# BloodVolumeLoss   Hematocrit \n");
   Init(0, 1.5);                 // set expected blood volume loss range [l]
   Run();                        // simulation run
 
-  // experiment 3: print value in terminal
-  //outputSelect = 3;
-  //Init(0, 0);                   // the Init is compulsory before Run()
-  //Run();                        // simulation run
+  // experiment 1_2:TODO  print value in terminal
   printf("experiment 3: ...\n");
+  printf("part 3: ANH transfusion after the operation...\n");
+  // compute number of erythrocytes withdrawn in the first phase of ANH
   double erythrocytesWithdrawn = hemodilutionWithdrawn.computeValue(40.0, 28.0, 5.0, 1.5);
+  // compute the final hematocrit after adding the withdrawn blood into patient's bloodstream
   printf("erythrocytesWithdrawn = %f\n", erythrocytesWithdrawn);
   double hematocritFinal = hemodilutionReplace.computeValue(17.5, erythrocytesWithdrawn, 3.5, 1.5);
   printf("hematocritFinal = %f\n", hematocritFinal);
-  //outputSelect++;
+
+  // TODO FIXME continue here... and don't forget to check the names of the experiments
 
   // experiment 1_3: 
-  outputSelect = 4;
+  outputSelect = 3;
   SetOutput("10_xsvobo1x_3.dat");
   Print("# Acute Normovolemic Hemodilution\n");
   Print("# BloodVolumeLoss   Hematocrit \n");
   Init(0, 0.5);                 // set expected blood volume loss range [l]
   Run();                        // simulation run
 
-
-  outputSelect = 9;
-  Init(0, 1.0);                   // set initial state and time
+  // experiment 2
+  outputSelect = 4;
+  SetOutput("10_xsvobo1x_4.dat");
+  Print("# Acute Normovolemic Hemodilution\n");
+  Print("# BloodVolumeLoss   Hematocrit \n");
+  Init(0, 1.5);                 // set expected blood volume loss range [l]
   Run();                        // simulation run
+
+  // experiment 3_1: 2 litres ANH withdrawal
+  outputSelect = 5;
+  SetOutput("10_xsvobo1x_5.dat");
+  Print("# Acute Normovolemic Hemodilution\n");
+  Print("# BloodVolumeLoss   Hematocrit \n");
+  Init(0, 2.0);                 // set expected blood volume loss range [l]
+  Run();                        // simulation run
+
+  // experiment 3_2: 1.5 litres blood volume loss during operation
+  outputSelect = 6;
+  SetOutput("10_xsvobo1x_6.dat");
+  Print("# Acute Normovolemic Hemodilution\n");
+  Print("# BloodVolumeLoss   Hematocrit \n");
+  Init(0, 1.5);                 // set expected blood volume loss range [l]
+  Run();                        // simulation run
+
+  // experiment 3_3: blood transfusion after operation
+  printf("experiment 3_3: ...\n");
+  erythrocytesWithdrawn = hemodilutionWithdrawn.computeValue(40.0, 24.0, 5.0, 2.0);
+  printf("erythrocytes withdrawn = %f\n", erythrocytesWithdrawn);
+  hematocritFinal = hemodilutionReplace.computeValue(19.5, erythrocytesWithdrawn, 3.0, 2.0);
+  printf("hematocrit final = %f\n", hematocritFinal);
+  // final hematocrit je 37.6 - cool
+
+
+  // experiment 4 - 2l replace, 1.5 during operation, no ANH
+  outputSelect = 7;
+  SetOutput("10_xsvobo1x_7.dat");
+  Print("# Acute Normovolemic Hemodilution\n");
+  Print("# BloodVolumeLoss   Hematocrit \n");
+  Init(0, 1.5);                 // set expected blood volume loss range [l]
+  Run();                        // simulation run
+  // final hematocrit je 28
+  // rozdil s ANH a bez ANH je velky - napsat do dokumentace presne tyto experimenty
+  // napsat vzorecky a postup, jakym se vse po sobe pocita - pri ANH i bez ANH
+  // udelat screenshoty
+
 
   SIMLIB_statistics.Output();   // print run statistics
 }
